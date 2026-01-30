e8ac53c235ed748f702fff38f23b02e036d4c538
ren3459 <ren3459@gmial.com>
2026-01-30 16:18:27 +0300

    fix readme, add test

--- tests/cards/cistern-card.test.ts
+++ tests/cards/cistern-card.test.ts
@@ -23,4 +23,36 @@ describe("cistern-card", () => {
     expect(el.shadowRoot).toBeTruthy();
     expect(el.shadowRoot!.textContent).toContain("Septic");
   });
+
+  it("renders without crashing when error_name config are provided", async () => {
+    const el = document.createElement(CISTERN_CARD_NAME) as CardTestElement;
+    el.setConfig({
+      type: `custom:${CISTERN_CARD_NAME}`,
+      entities: ENTITIES,
+      error_name: { show: true, label: "Error" },
+    });
+    el.hass = createHass();
+
+    document.body.appendChild(el);
+    await el.updateComplete;
+
+    expect(el.shadowRoot).toBeTruthy();
+    expect(el.shadowRoot!.textContent).toContain("Error");
+  });
+
+  it("renders without crashing when default config", async () => {
+    const el = document.createElement(CISTERN_CARD_NAME) as CardTestElement;
+    el.setConfig({
+      type: `custom:${CISTERN_CARD_NAME}`,
+      entities: ENTITIES,
+    });
+    el.hass = createHass();
+
+    document.body.appendChild(el);
+    await el.updateComplete;
+
+    expect(el.shadowRoot).toBeTruthy();
+    expect(el.shadowRoot!.textContent).toContain("Temperature");
+    expect(el.shadowRoot!.textContent).toContain("Pressure");
+  });
 });
