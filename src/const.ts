d35493b9832d30f0c216fb32fc6b0eabaef7af95
arufanov <arufanov@gmail.com>
2026-02-03 13:26:43 +0300

    The ru supported as default until i18n implementation (#171)
    The ru supported as default until i18n implementation (#171)

--- src/const.ts
+++ src/const.ts
@@ -10,6 +10,17 @@ export const TILE_CARD_NAME = `${CARD_PREFIX}-tile-card` as const;
 
 export const SEPTIC_DIALOG_NAME = `${CARD_PREFIX}-dialog` as const;
 
+/*
+ * Default language is EN. This is workaround until i18n implementation.
+ *
+  header: { show: false, label: "Septic" },
+  level: { show: false, icon: "mdi:water-percent", label: "Liquid level" },
+  temp: { show: true, icon: "mdi:thermometer", label: "Temperature" },
+  pressure: { show: true, icon: "mdi:gauge", label: "Pressure" },
+  x_level: { show: false, icon: "mdi:water-alert", label: "Critical level" },
+  exceeds_x_level: { show: false, icon: "mdi:alert-octagon-outline", label: "Exceeding the liquid level" },
+  error_name: { show: false, icon: "mdi:alert-circle-outline", label: "Error" },
+ */
 export const SEPTIC_CARD_DEFAULT_CONFIG: Readonly<SepticCardConfig> = {
   entities: {
     level: "level",
@@ -22,10 +33,10 @@ export const SEPTIC_CARD_DEFAULT_CONFIG: Readonly<SepticCardConfig> = {
 
   type: `custom:${CISTERN_CARD_NAME}`,
   header: { show: false, label: "Septic" },
-  level: { show: false, icon: "mdi:water-percent", label: "Liquid level" },
-  temp: { show: true, icon: "mdi:thermometer", label: "Temperature" },
-  pressure: { show: true, icon: "mdi:gauge", label: "Pressure" },
-  x_level: { show: false, icon: "mdi:water-alert", label: "Critical level" },
-  exceeds_x_level: { show: false, icon: "mdi:alert-octagon-outline", label: "Exceeding the liquid level" },
-  error_name: { show: false, icon: "mdi:alert-circle-outline", label: "Error" },
+  level: { show: false, icon: "mdi:water-percent", label: "Уровень жидкости" },
+  temp: { show: true, icon: "mdi:thermometer", label: "Температура" },
+  pressure: { show: true, icon: "mdi:gauge", label: "Давление" },
+  x_level: { show: false, icon: "mdi:water-alert", label: "Критический уровень" },
+  exceeds_x_level: { show: false, icon: "mdi:alert-octagon-outline", label: "Превышение уровня" },
+  error_name: { show: false, icon: "mdi:alert-circle-outline", label: "Ошибка" },
 };
