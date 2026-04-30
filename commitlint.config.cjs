af9ae542ecff4884695c7554dc0828a4161fa4d9
arufanov <arufanov@gmail.com>
2026-04-30 13:29:48 +0300

    feat(ci): add commit conventions linter

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'subject-case': [2, 'always', 'lower-case'],
+    'subject-full-stop': [2, 'never', '.'],
+  },
+};
